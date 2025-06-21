b824974ffa6f6c9adfcb68e118a37eaffd8dfd9d
pablothecoder <ortegaperaltap@gmail.com>
2025-06-20 21:52:10 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,3 +6,17 @@ const contentDisplayBox = document.querySelector('#contentDisplayBox')
 
 const refreshBtn = document.querySelector('#refreshBtn')
 const retryBtn = document.querySelector('#retryBtn')
+
+const getRepository = async () => {
+  try {
+    const config = { headers: { Accept: 'application/vnd.github+json' } }
+    const res = await axios.get(`https://api.github.com/search/repositories?q=${languageMenu.value}`, config)
+    console.log(res)
+  }
+  catch (error) {
+    console.log(error)
+  }
+
+}
+
+getRepository()
