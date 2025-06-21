73754d030f310067d62727efc5c1ee2a76aa3103
pablothecoder <ortegaperaltap@gmail.com>
2025-06-20 21:28:46 -0400

    html css and js files updated

---
+++ app.js
@@ -0,0 +1,8 @@
+const body = document.querySelector('body')
+const darkModeBtn = document.querySelector('#toggleDarkModeBtn')
+
+const languageMenu = document.querySelector('#languageSelect')
+const contentDisplayBox = document.querySelector('#contentDisplayBox')
+
+const refreshBtn = document.querySelector('#refreshBtn')
+const retryBtn = document.querySelector('#retryBtn')
