f95c9a5a0d1fc2cca11a77e2898e0a95f8c6c801
pablothecoder <ortegaperaltap@gmail.com>
2025-06-21 11:14:19 -0400

    html and js files updated

--- app.js
+++ app.js
@@ -3,20 +3,31 @@ const darkModeBtn = document.querySelector('#toggleDarkModeBtn')
 
 const languageMenu = document.querySelector('#languageSelect')
 const contentDisplayBox = document.querySelector('#contentDisplayBox')
+const contentDisplayBoxTitle = document.querySelector('#contentDisplayBox h2')
 
 const refreshBtn = document.querySelector('#refreshBtn')
 const retryBtn = document.querySelector('#retryBtn')
 
-const getRepository = async () => {
+// Search repositories api
+
+const getRandomRepoByLang = async (lang) => {
   try {
     const config = { headers: { Accept: 'application/vnd.github+json' } }
-    const res = await axios.get(`https://api.github.com/search/repositories?q=${languageMenu.value}`, config)
-    console.log(res)
+    const res = await axios.get(`https://api.github.com/search/repositories?q=${lang}`, config)
+    const filteredRepos = res.data.items.filter(repo => repo.language === lang)
+    const randomRepo = filteredRepos[Math.floor(Math.random() * 16)]
+    console.log(randomRepo)
   }
   catch (error) {
     console.log(`Error: ${error}`)
   }
-
 }
 
-getRepository()
+getRandomRepoByLang('JavaScript')
+
+languageMenu.addEventListener('click', (e) => {
+  contentDisplayBoxTitle.classList.add('hidden')
+})
+
+
+// res.data.items[Math.floor(Math.random() * 16)]
