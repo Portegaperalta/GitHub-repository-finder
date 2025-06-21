3d457d3ae3a87f8b11b451a3679b11fa4dae62e6
pablothecoder <ortegaperaltap@gmail.com>
2025-06-21 11:36:49 -0400

    Update app.js

--- app.js
+++ app.js
@@ -12,18 +12,46 @@ const retryBtn = document.querySelector('#retryBtn')
 
 const getRandomRepoByLang = async (lang) => {
   try {
+
+    //fetch random repo from github
+
     const config = { headers: { Accept: 'application/vnd.github+json' } }
     const res = await axios.get(`https://api.github.com/search/repositories?q=${lang}`, config)
     const filteredRepos = res.data.items.filter(repo => repo.language === lang)
     const randomRepo = filteredRepos[Math.floor(Math.random() * 16)]
     console.log(randomRepo)
+
+    //creating elements to add to dom
+
+    const newRepoTitle = document.createElement('h3')
+    const newRepoDescription = document.createElement('p')
+    const newRepoLang = document.createElement('p')
+    const newRepoStars = document.createElement('p')
+    const newRepoForks = document.createElement('p')
+    const newRepoWatchers = document.createElement('p')
+
+    //asigning value to elements
+
+    newRepoTitle.innerText = randomRepo.full_name
+    newRepoDescription.innerText = randomRepo.description.slice(0, 50)
+    newRepoLang.innerText = randomRepo.language
+    newRepoStars.innerText = randomRepo.stargazers_count
+    newRepoForks.innerText = randomRepo.forks
+    newRepoWatchers.innerText = randomRepo.watchers_count
+
+
+    //appending elements to dom 
+    contentDisplayBox.append(newRepoTitle)
+    contentDisplayBox.append(newRepoDescription)
+    contentDisplayBox.append(newRepoLang)
+    contentDisplayBox.append(newRepoStars)
+    contentDisplayBox.append(newRepoForks)
+    contentDisplayBox.append(newRepoWatchers)
   }
   catch (error) {
     console.log(`Error: ${error}`)
   }
 }
-
-getRandomRepoByLang('JavaScript')
 
 languageMenu.addEventListener('click', (e) => {
   contentDisplayBoxTitle.classList.add('hidden')
